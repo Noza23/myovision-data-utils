ea04dd2d0b905e78be35d1844c9c86a31403597c
Noza23 <G.Nozadze@campus.lmu.de>
2024-02-24 19:24:19 +0100

    feat: dataclass validation

--- merge_roi.py
+++ merge_roi.py
@@ -1,15 +1,35 @@
-import argparse, os
+import os
+import argparse
+from dataclasses import dataclass
+
+import read_roi
+import numpy as np
+import cv2
 
 from generator_backend.utils import (
     mask_to_rle,
     save_rle_masks,
-    read_masks
+    read_masks,
+    rle_to_mask,
 )
 
-import read_roi
-import numpy as np
-from segment_anything.utils.amg import rle_to_mask
-import cv2
+
+@dataclass
+class Arguments:
+    masks: str
+    roi: str
+    output: str
+    mask_suffix: str
+    roi_suffix: str
+
+    def __post_init__(self):
+        if not os.path.exists(self.masks):
+            raise FileNotFoundError(f"Masks file not found: {self.masks}")
+        if not os.path.exists(self.roi):
+            raise FileNotFoundError(f"Roi file not found: {self.roi}")
+        if not os.path.exists(self.output):
+            os.makedirs(self.output)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Merge ROI masks to existing ones")
@@ -21,12 +41,13 @@ if __name__ == "__main__":
     )
     parser.add_argument("--roi_suffix", type=str, default=".zip")
 
-    args = parser.parse_args()
+    args = Arguments(**vars(parser.parse_args()))
 
     mask_files = os.listdir(args.masks)
     roi_files = os.listdir(args.roi)
     matching_files = [
-        f.replace(args.mask_suffix, "") for f in mask_files
+        f.replace(args.mask_suffix, "")
+        for f in mask_files
         if f.replace(args.mask_suffix, "") + args.roi_suffix in roi_files
     ]
     print("Matching files found: ", matching_files)
@@ -35,10 +56,10 @@ if __name__ == "__main__":
         print("> Processing: ", f)
         mask_file = os.path.join(args.masks, f + args.mask_suffix)
         roi_file = os.path.join(args.roi, f + args.roi_suffix)
-        
+
         masks_json = read_masks(mask_file)
         masks_np = [rle_to_mask(mask) for mask in masks_json]
-    
+
         rois = read_roi.read_roi_zip(roi_file)
         binary_roi_masks = []
         binary_roi_mask = np.zeros_like(masks_np[0], dtype=np.uint8)
@@ -53,7 +74,7 @@ if __name__ == "__main__":
                 print(f"in {f} roi with key {key} failed: ")
                 raise e
             binary_roi_masks.append(mask.astype(np.bool_))
-        
+
         masks_np.extend(binary_roi_masks)
         rle_masks = mask_to_rle(
             np.stack(masks_np, axis=0), batch_size=10, device="cpu"
@@ -62,5 +83,5 @@ if __name__ == "__main__":
         mask_fn = os.path.join(args.output, f + "_mask_merged.json")
         save_rle_masks(rle_masks, mask_fn)
         print("RLE masks saved to: ", mask_fn)
-    
+
     print("Mergning Masks completed!")
